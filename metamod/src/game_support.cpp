d45f7e2bb15ed9ad5c6cde6633416189d89035e3
STAM <stamepicmorg@users.noreply.github.com>
2017-08-05 03:11:41 +0300

    Desert Crisis

--- metamod/src/game_support.cpp
+++ metamod/src/game_support.cpp
@@ -8,7 +8,7 @@ const game_modinfo_t g_known_games[] = {
 	// Previously enumerated in this sourcefile, the list is now kept in a
 	// separate file, generated based on game information stored in a
 	// convenient db.
-	{ "action",             "ahl_i386.so",              "ahl.dll",                   "Action Half-Life"                                 }, // + director's cut [2016]
+	{ "action",             "ahl_i386.so",              "ahl.dll",                   "Action Half-Life"                                 }, // + director's cut [2016], updated linux binary name
 	{ "ag",                 "ag.so",                    "ag.dll",                    "Adrenaline Gamer"                                 }, // .so file by OpenAG fork
 	{ "asheep",             nullptr,                    "hl.dll",                    "Azure Sheep"                                      }, // have no linux binary found!
 	{ "bdef",               "server.so",                "server.dll",                "Base Defense"                                     }, // placed in normal dll folder [2017]
@@ -24,8 +24,8 @@ const game_modinfo_t g_known_games[] = {
 	{ "cstrike",            "cs.so",                    "mp.dll",                    "Counter-Strike 1.6"                               },
 	{ "czero",              "cs.so",                    "mp.dll",                    "Counter-Strike:Condition Zero"                    },
 	{ "czeror",             "cz.so",                    "cz.dll",                    "Counter-Strike:Condition Zero Deleted Scenes"     },
-	{ "dcrisis",            "dc.so",                    "dc.dll",                    "Desert Crisis"                                    },
-	{ "decay",              nullptr,                    "decay.dll",                 "Half-Life: Decay"                                 },
+	{ "dcrisis",            "dc_i386.so",               "dc.dll",                    "Desert Crisis"                                    }, // updated linux binary name [2010]
+	{ "decay",              nullptr,                    "decay.dll",                 "Half-Life: Decay"                                 }, // have no linux binary!
 	{ "dmc",                "dmc.so",                   "dmc.dll",                   "Deathmatch Classic"                               },
 	{ "dod",                "dod.so",                   "dod.dll",                   "Day of Defeat"                                    },
 	{ "dpb",                "pb.i386.so",               "pb.dll",                    "Digital Paintball"                                },
